debcb1eaf22d131d076fdef4aba2f5aba59c8e71
Kal-Elx <kallej1johansson@gmail.com>
2018-11-24 12:07:13 +0100

    Solution to Babelfish. Runtime: 0.25 s.

---
+++ Kattis/Babelfish/babelfish.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map<string, string> dictionary;
+    string input;
+    string englishWord;
+    string foreignWord;
+
+    while (getline(cin, input)) {
+        if (input == "") {
+            break;
+        }
+
+        size_t x = input.find(" ");
+        englishWord = input.substr(0, x);
+        foreignWord = input.substr(x+1, input.length()-1); 
+        
+        dictionary[foreignWord] = englishWord;
+    }
+
+    while (cin >> foreignWord) {
+        cout << (dictionary.find(foreignWord) != dictionary.cend() ? dictionary[foreignWord] : "eh") << endl;
+    }
+
+    return 0;
+}
